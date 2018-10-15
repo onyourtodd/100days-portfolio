97e4aec20840ca632d5b0dfc6a488c3e64318b29
Todd Fraser <toddfraser@Todds-iMac.local>
2018-10-15 19:08:16 +0200

    Refactored mainBody.js link section to iterate through list and take links and name from data keys

--- src/components/mainbody.js
+++ src/components/mainbody.js
@@ -1,14 +1,27 @@
 import React from 'react'
 
+const MainLinks = [
+  {
+    name: 'Latest Work',
+    link: 'http://www.email-mentor.com/'
+  }, {
+    name: 'Contact',
+    link: 'mailto:kron_todd@hotmail.com'
+  }
+]
+
 const MainBody = ({ children }) => (
   <section className="main-wrapper">
     <h1>Hola</h1>
     <h2>I am Todd, a <span>front-end</span> <span id="js-rotating">developer, designer, consultant</span> from London, living in 🌞 Barcelona.</h2>
-    <p>  I have over 10 years experience in the digital space, across a variety of different departments, projects and disciplines. Right now, I am currently focused on creating web apps using the latest front end technologies.</p>
+    <p> I have over 10 years experience in the digital space, across a variety of different departments, projects and disciplines. Right now, I am currently focused on creating web apps using the latest front end technologies.</p>
     <p>Including but not limited to; <i>HTML5</i>, <i>CSS3</i>, <i>Javascript</i>, <i>React</i>, <i>Redux</i>.</p>
     <ul>
-      <li><a href="http://www.theriahgroup.co.uk">Latest work <span>&rarr;</span></a></li>
-      <li><a href="mailto:kron_todd@hotmail.com">Contact <span>&rarr;</span></a></li>
+      {MainLinks.map( link => (
+        <li>
+          <a href={link.link} target="_blank" rel="noopener noreferrer nofollow">{link.name} <span>&rarr;</span></a>
+        </li>
+      ))}
     </ul>
   </section>
 )
